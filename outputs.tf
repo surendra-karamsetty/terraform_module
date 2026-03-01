5e605a7db9c9ef21e34c83596ed44756c96b73a4
surendra-karamsetty <surendra3207@gmail.com>
2026-03-01 18:45:17 +0530

    Commit Changes

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "instance_id" {
     value = aws_instance.main.id
 }
 
-output "private_id" {
-    value = aws_instance.main.private_id
+output "private_ip" {
+    value = aws_instance.main.private_ip
 }
 
-output "public_id" {
-    value = aws_instance.main.public_id
+output "public_ip" {
+    value = aws_instance.main.public_ip
 }
